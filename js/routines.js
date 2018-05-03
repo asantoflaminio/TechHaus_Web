0f6e9af91d595abeab84a11b7191e1189791ce32
florgiorgi <mgiorgi@itba.edu.ar>
2018-05-03 20:53:15 -0300

    Update routines.js

--- js/routines.js
+++ js/routines.js
@@ -27,7 +27,7 @@ for (i = 0; i < acc.length; i++) {
 
 function play(name, event){
     event.stopPropagation();
-    name = name.nextSibling.nextSibling.innerHTML
+    name = name.nextSibling.nextSibling.innerHTML;
     var notice = document.getElementsByClassName('notice')[0];
     notice.style.display = 'block';
     document.getElementById('name').innerHTML = "Routine \"" + name + "\" was played";
@@ -37,3 +37,80 @@ function play(name, event){
     }, 3000);
     
 }
+
+//add routine
+
+$(function() {
+    
+    function edit(event, title){
+    var divHtml = title.html();
+    var editableText = $("<textarea />");
+    editableText.val(divHtml);
+    title.replaceWith(editableText);
+    editableText.focus();
+    // setup the blur event for this new textarea
+    editableText.blur(editableTextBlurred(title));
+    }
+
+    function editableTextBlurred(title) {
+    var html = title.val();
+    var viewableText = $("<div>");
+    viewableText.html(html);
+    title.replaceWith(viewableText);
+    // setup the click event for this new div
+    $(viewableText).click(divClicked);
+    }
+    
+$("#addicon").click(function() {
+
+    $("#popup").dialog({
+        modal: true,
+        buttons: [ 
+            {   
+                text:'Cancel',
+                "class": "mybutton cancel-button",
+                click: function() {
+
+                $(this).dialog('close');
+                }
+            },
+            {
+                text: 'Add',
+                "class": "mybutton",
+                click: function () {
+
+                var list = document.getElementById("routines_list");
+                var elem = document.createElement("div");
+                elem.setAttribute("class", "accordion");
+                var div = document.createElement("div");
+                var img1 = document.createElement("img");
+                var img2 = document.createElement("img");
+                var h3 = document.createElement("h3");
+
+                img1.setAttribute("src", "Iconos/arrow_down.png");
+                img1.setAttribute("alt", "Expand");
+                img1.setAttribute("class", "arrow_icon");
+
+                img2.setAttribute("src", "Iconos/play.png");
+                img2.setAttribute("alt", "Play");
+                img2.setAttribute("class", "play_icon");
+                img2.setAttribute("onclick", "play(this,event)");
+
+                h3.setAttribute("class", "routine_name");
+                h3.innerHTML = $('#routine_input').val();
+
+                div.appendChild(img1);
+                div.appendChild(img2);
+                div.appendChild(h3);
+                elem.appendChild(div);
+                list.appendChild(elem);
+
+                $(this).dialog('close');
+
+                }
+            }
+        ]
+    })
+})
+
+});
