625212ac7b9d261e43bd6a4402b516f385e996ab
Alejandro Santoflaminio <asantoflaminio@itba.edu.ar>
2018-05-05 14:39:21 -0300

    Help ya con 4 categorias
    Columna izquierda sigue fea
    Scroll medio raro

--- js/help.js
+++ js/help.js
@@ -1,9 +1,60 @@
 function showFavorites() {
     // Declare variables
-    var logo, i, favs;
+    var logo,  favs, gs, dl, ad;
     logo = document.getElementById("logogrande");
     logo.style.display = "none";
-
+    gs = document.getElementById("gstarted");
+    gs.style.display = "none";
+    dl = document.getElementById("Delete");
+    dl.style.display = "none";
+    ad = document.getElementById("Adding");
+    ad.style.display = "none";
     favs = document.getElementById("Favs");
     favs.style.display = "block";
+}
+
+function showGettingStarted() {
+    // Declare variables
+    var logo,  gs, favs, dl, ad;
+    logo = document.getElementById("logogrande");
+    logo.style.display = "none";
+    favs = document.getElementById("Favs");
+    favs.style.display = "none";
+    dl = document.getElementById("Delete");
+    dl.style.display = "none";
+    ad = document.getElementById("Adding");
+    ad.style.display = "none";
+    gs = document.getElementById("gstarted");
+    gs.style.display = "block";
+    
+}
+
+function showDeleting() {
+    // Declare variables
+    var logo,  gs, favs, dl, ad;
+    logo = document.getElementById("logogrande");
+    logo.style.display = "none";
+    favs = document.getElementById("Favs");
+    favs.style.display = "none";
+    gs = document.getElementById("gstarted");
+    gs.style.display = "none";
+    ad = document.getElementById("Adding");
+    ad.style.display = "none";
+    dl = document.getElementById("Delete");
+    dl.style.display = "block";
+}
+
+function showAdding() {
+    // Declare variables
+    var logo,  gs, favs, dl, ad;
+    logo = document.getElementById("logogrande");
+    logo.style.display = "none";
+    favs = document.getElementById("Favs");
+    favs.style.display = "none";
+    gs = document.getElementById("gstarted");
+    gs.style.display = "none";
+    dl = document.getElementById("Delete");
+    dl.style.display = "none";
+    ad = document.getElementById("Adding");
+    ad.style.display = "block";
 }
