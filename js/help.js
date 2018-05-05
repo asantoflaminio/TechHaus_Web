1953015a072e4fbbfa7f304c26660ce3859817db
Alejandro Santoflaminio <asantoflaminio@itba.edu.ar>
2018-05-05 01:33:48 -0300

    Agregado Help con Favorites
    Error con el scroll. 
    Ademas hay una caja duplicada que use para probar el scroll.

---
+++ js/help.js
@@ -0,0 +1,9 @@
+function showFavorites() {
+    // Declare variables
+    var logo, i, favs;
+    logo = document.getElementById("logogrande");
+    logo.style.display = "none";
+
+    favs = document.getElementById("Favs");
+    favs.style.display = "block";
+}
