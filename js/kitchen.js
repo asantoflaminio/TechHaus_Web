696a3a8b8902abfddd168f2711ac551f20831ad4
Alejandro Santoflaminio <asantoflaminio@itba.edu.ar>
2018-05-13 15:25:42 -0300

    Se pueden agregar rooms. En kitchen se pueden agregar devices.
    ERROR! : Rooms mal ordenados, feo
    ERROR! : Add no cierra cuando agrega en ambos
    --Por ahora todos los rooms van a kitchen--

--- js/kitchen.js
+++ js/kitchen.js
@@ -67,7 +67,130 @@ api.devicetypes = class {
 //TODO
 
 function add_device(event, name) {
+                var list = document.getElementById("devices_list");
+                var elem = document.createElement("div");
+                elem.setAttribute("class", "accordion");
+                var div1 = document.createElement("div");
+                var div2 = document.createElement("div");
+                var div3 = document.createElement("div");
+                var div4 = document.createElement("div");
+                var div5 = document.createElement("div");
+                var div6 = document.createElement("div");
+                var div7 = document.createElement("div");
+                var div8 = document.createElement("div");
+                var p = document.createElement("p");
+                var h3 = document.createElement("h3");
+                var img = document.createElement("img");
+                var trash = document.createElement("img");
+                var heart = document.createElement("img");
+                var yes = document.createElement("img");
+                var no = document.createElement("img");
+                var pencil2 = document.createElement("img");
+                var h4 = document.createElement("h4");
+                img.setAttribute("src", "Iconos/arrow_down.png");
+                img.setAttribute("alt", "Expand");
+                img.setAttribute("class", "arrow_icon");
+                trash.setAttribute("src", "Iconos/tacho.png");
+                trash.setAttribute("alt", "Delete");
+                trash.setAttribute("class", "delete_icon");
+                trash.setAttribute("onclick", "trash(event,this);");
+                heart.setAttribute("src", "Iconos/heart.png");
+                heart.setAttribute("alt", "Fave");
+                heart.setAttribute("class", "fave_icon");
+                heart.setAttribute("onclick", "fav(event,this);");
+                yes.setAttribute("src", "Iconos/yes.png");
+                yes.setAttribute("alt", "Yes");
+                yes.setAttribute("class", "yes_icon");
+                yes.setAttribute("onclick", "yes(event,this);");
+                no.setAttribute("src", "Iconos/no.png");
+                no.setAttribute("alt", "No");
+                no.setAttribute("class", "no_icon");
+                no.setAttribute("onclick", "no(event,this);");
+                pencil2.setAttribute("src", "Iconos/pencil.png");
+                pencil2.setAttribute("alt", "Pencil;");
+                pencil2.setAttribute("class", "pencil2_icon");
+                    
+                div1.appendChild(img);
+                div4.appendChild(trash);
+                div5.appendChild(heart);
+                div6.appendChild(yes);
+                div7.appendChild(no);
+                div8.appendChild(h4);
+                h4.setAttribute("class", "trash_message");
+                h4.innerHTML = "You are about to delete this device. Continue? ";
+                h3.setAttribute("class", "device_name");
+                h3.innerHTML = $('#device_input').val();
+                div2.appendChild(h3);
+                div2.appendChild(pencil2);
+                div2.setAttribute("onmouseover", "pencil2_display(event,this);");
+                div2.setAttribute("onmouseout","pencil2_out(event,this);");   
+                p.setAttribute("class", "device_room");
+                p.innerHTML = $('#room_input').val();
+                var div9 = document.createElement("div");
+                var p1 = document.createElement("p");
+                p1.innerHTML = "Info on";
+                div9.setAttribute("class", "panel");
+                div3.appendChild(p);
+                elem.appendChild(div1);
+                
+                elem.appendChild(div5);
+                elem.appendChild(div4);
+                elem.appendChild(div6);
+                elem.appendChild(div7);
+                elem.appendChild(div2);
+                elem.appendChild(div3);
+                elem.appendChild(div8);
+                elem.appendChild(div9);
+                
+                list.appendChild(elem);
+                div9.appendChild(p1);
+                list.appendChild(div9);
 
+                elem.addEventListener("click", function() {
+                var panel = this.nextElementSibling;
+                if (panel.style.display === "block") {
+                    panel.style.display = "none";
+                } else {
+                    panel.style.display = "block";
+                }
+                var arrow = this.children[0].children[0];
+                if (panel.style.display === "block") {
+                    arrow.src = "Iconos/arrow_up.png";
+                } else {
+                    arrow.src = "Iconos/arrow_down.png";
+                }
+                });
+                    
+                api.rooms.getRooms().done(function(data) {
+
+                            $.each(data, function(i, item){
+                                if(item.name.toUpperCase() == document.getElementById("title").innerHTML.toUpperCase()){
+                                    api.devicetypes.getDeviceTypes().done(function(data) {
+                                        $.each(data, function(j, item2){
+                                            if(item2.name.toUpperCase() == $('#room_input').val().toUpperCase()){
+                                               api.devices.addDevice(item2.id,$('#device_input').val(),item.name).done(function(data) {
+                                                   api.devices.getDevices().done(function(data){
+                                                       $.each(data, function(r, item3){
+                                                       if($('#device_input').val() == item3.name){
+                                                           console.log("VOY A LINKEAR");
+                                                           api.devices.link(item3.id, item.id);
+                                                       }
+                                                       });
+                                                   });
+                                                   
+                                               });
+                                                
+                                               }
+                                            
+                                        });
+                                        
+                                    });
+                                    
+                                }
+                            });
+                });
+              //  document.getElementById("mymodal").modal('hide');
+                //$(this).dialog('close');
 }
 
 api.devices = class {
--- js/rooms.js
+++ js/rooms.js
@@ -25,6 +25,17 @@ api.rooms = class {
        
      
   }
+  static postRoom(name2, iconname) {
+   return $.ajax({
+      url: api.rooms.url,
+      method: "POST",
+      dataType: "json",
+      timeout: api.timeout,
+       data: {'name': name2,  'meta': "{" + iconname + "}"},
+       });
+       
+     
+  }
 }
 
 $(document).ready(function() {
@@ -101,4 +112,58 @@ function trash_out(event, title){
    var trash = title.closest('div').querySelector('.trash_icon');
     trash.style.visibility = "hidden";
     
+}
+
+function add_room(event, name) {
+        var roomname = $('#name_input').val();//JSON.stringify(item.name.replace("\"",""));
+        console.log(roomname);
+        var roomtype = $('#room_input').val().toLowerCase();//tomo el primer elemento de meta
+        console.log(roomtype);
+        var icons = document.getElementById("icons");
+        var fig = document.createElement("figure");
+        var a_elem = document.createElement("a"); 
+        var div1 = document.createElement("div");
+        var img1 = document.createElement("img");
+        var img2 = document.createElement("img");   
+        var fig_cap = document.createElement("figcaption");
+        
+        div1.setAttribute("onmouseover", "trash_display(event,this);");
+        div1.setAttribute("onmouseout", "trash_out(event,this);");
+        div1.setAttribute("class", "roomandtrash");     
+        a_elem.setAttribute("href", "kitchen.html"); //despues va a cambiar
+        if(roomtype == "bathroom"){
+            img1.setAttribute("src", "Iconos/bathroom2.png"); 
+        }else if(roomtype == "garage"){
+            img1.setAttribute("src", "Iconos/garage2.png"); 
+        }else if(roomtype == "kitchen"){
+             img1.setAttribute("src", "Iconos/kitchen1.png");      
+        }else if(roomtype == "garden"){
+            img1.setAttribute("src", "Iconos/jardin4.png");   
+        }else if(roomtype == "laundry"){
+            img1.setAttribute("src", "Iconos/laundry2.png");      
+        }else if(roomtype == "bedroom"){
+            img1.setAttribute("src", "Iconos/Rooms6.png");       
+        }else{
+            img1.setAttribute("src", "Iconos/marta.png");     
+        }
+        img1.setAttribute("alt", roomname);
+        img1.setAttribute("class", "icon");
+        img2.setAttribute("src", "Iconos/tacho.png");
+        img2.setAttribute("alt", "Trash");
+        img2.setAttribute("class", "trash_icon");
+        img2.setAttribute("onclick", "display_box(event,this);");
+        fig_cap.innerHTML = roomname;
+        
+        div1.appendChild(img1);
+        div1.appendChild(img2);
+        a_elem.appendChild(div1);
+        fig.appendChild(a_elem);
+        fig.appendChild(fig_cap);
+        icons.appendChild(fig);
+    
+        api.rooms.postRoom(roomname,roomtype).done(function(data) {
+                                                   
+                                                           console.log("ROOM ADDED");
+                                                       });
+
 }
