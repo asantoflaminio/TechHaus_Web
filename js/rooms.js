db23897a32facedfa910f5168f9a4c81626c00d9
Alejandro Santoflaminio <asantoflaminio@itba.edu.ar>
2018-05-13 17:28:40 -0300

    Arreglado un error de agregar rooms

--- js/rooms.js
+++ js/rooms.js
@@ -58,7 +58,7 @@ $(document).ready(function() {
         div1.setAttribute("onmouseout", "trash_out(event,this);");
         div1.setAttribute("class", "roomandtrash");     
         a_elem.setAttribute("href", "kitchen.html"); //despues va a cambiar
-        a_elem.setAttribute("onclick", "passId(event, this)")
+        a_elem.setAttribute("onclick", "passId(event, this)");
         if(roomtype == "bathroom"){
             img1.setAttribute("src", "Iconos/bathroom2.png"); 
         }else if(roomtype == "garage"){
@@ -132,6 +132,7 @@ function add_room(event, name) {
         div1.setAttribute("onmouseout", "trash_out(event,this);");
         div1.setAttribute("class", "roomandtrash");     
         a_elem.setAttribute("href", "kitchen.html"); //despues va a cambiar
+        a_elem.setAttribute("onclick", "passId(event, this)");
         if(roomtype == "bathroom"){
             img1.setAttribute("src", "Iconos/bathroom2.png"); 
         }else if(roomtype == "garage"){
