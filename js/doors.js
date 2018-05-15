be42d18e528a8b88c8c766221761620273fae246
florgiorgi <mgiorgi@itba.edu.ar>
2018-05-15 15:22:37 -0300

    borrar con popup

--- js/doors.js
+++ js/doors.js
@@ -155,6 +155,17 @@ api.devices = class {
            return data.devices; //.map(item => item.name);
         });      
   }
+
+      static deleteDevice(deviceId) {
+   return $.ajax({
+      url: api.devices.url + deviceId,
+      method: "DELETE",
+      dataType: "json",
+      timeout: api.timeout,
+       })
+       
+     
+  }
 }
 
 
@@ -252,7 +263,7 @@ function change_blind_status_from_acc(event, status){
                 }
 }
 
-function trash(event, trashcan){
+/*function trash(event, trashcan){
     event.stopPropagation();
     if (trashcan.getAttribute('src') == "Iconos/tacho.png")
                 {
@@ -272,7 +283,32 @@ function trash(event, trashcan){
                     no.style.visibility = "visible";
                     
                 }
+}*/
+
+function trash(event, tacho){
+    var devicename = tacho.closest('div').parentNode.parentNode.querySelector('.device_name').innerHTML;
+    var deviceid = "";
+    //window.localStorage.clear();
+    window.localStorage.setItem("devicename", devicename);
+    api.devices.getAllDevices().done(function(data){
+        $.each(data, function(i, item){
+            if(item.name == devicename){
+                deviceid = item.id;
+                document.getElementById("msg-tag").innerHTML = "You are about to delete device \'"+devicename+ "\'."
+                window.localStorage.setItem("device_id2", deviceid);
+            }
+        });
+    });  
 }
+
+function delete_device(event, confirm){
+    var deviceid = window.localStorage.getItem("device_id2");
+    console.log("estoy aca" + deviceid);
+    api.devices.deleteDevice(deviceid).done(function(data){
+        onPageLoad();                                           
+    });
+};
+
 
 function no(event, noicon){
     event.stopPropagation();
@@ -416,7 +452,7 @@ function onPageLoad(){
         myNode.removeChild(myNode.firstChild);
     }
     var typeid = window.localStorage.getItem("type_id");
-    console.log("desde doors.js tengo typeid = " + typeid);
+//    console.log("desde doors.js tengo typeid = " + typeid);
     var typename = window.localStorage.getItem("type_name");
     var logo = document.getElementById("deviceicon");
     var brd = document.getElementById("breadcrumbfinal");
@@ -719,6 +755,8 @@ function onPageLoad(){
             trash.setAttribute("alt", "Delete");
             trash.setAttribute("class", "delete_icon");
             trash.setAttribute("onclick", "trash(event,this);");
+            trash.setAttribute("data-toggle", "modal");
+            trash.setAttribute("data-target", "#delete_device_popup");
             yes.setAttribute("src", "Iconos/yes.png");
             yes.setAttribute("alt", "Yes");
             yes.setAttribute("class", "yes_icon");
@@ -1676,115 +1714,6 @@ $(function() {
     title.replaceWith(viewableText);
     // setup the click event for this new div
     $(viewableText).click(divClicked);
-    }
-    
-    
-    
-    
-   //esto es viejo, borrar (creo)
-$("#addicon").click(function() {
-
-  $("#popup").dialog({
-    modal: true,
-      buttons: [ 
-        { 
-          text:'Cancel',
-          "class": "mybutton cancel-button",
-          click: function() {
-
-          $(this).dialog('close');
-          }
-        },
-        {
-          text: 'Add',
-          "class": "mybutton",
-          click: function () {
-          var list = document.getElementById("doors_list");
-          var elem = document.createElement("div");
-          elem.setAttribute("class", "accordion");
-          var div1 = document.createElement("div");
-          var div2 = document.createElement("div");
-          var div3 = document.createElement("div");
-                var div4 = document.createElement("div");
-                var div5 = document.createElement("div");
-                var div6 = document.createElement("div");
-                var div7 = document.createElement("div");
-                var div8 = document.createElement("div");
-          var p = document.createElement("p");
-          var h3 = document.createElement("h3");
-          var img = document.createElement("img");
-                var trash = document.createElement("img");
-                var heart = document.createElement("img");
-                var yes = document.createElement("img");
-                var no = document.createElement("img");
-                var pencil2 = document.createElement("img");
-                var h4 = document.createElement("h4");
-          img.setAttribute("src", "Iconos/arrow_down.png");
-          img.setAttribute("alt", "Expand");
-          img.setAttribute("class", "arrow_icon");
-                trash.setAttribute("src", "Iconos/tacho.png");
-          trash.setAttribute("alt", "Delete");
-          trash.setAttribute("class", "delete_icon");
-                trash.setAttribute("onclick", "trash(event,this);");
-                heart.setAttribute("src", "Iconos/heart.png");
-          heart.setAttribute("alt", "Fave");
-          heart.setAttribute("class", "fave_icon");
-                heart.setAttribute("onclick", "fav(event,this);");
-                yes.setAttribute("src", "Iconos/yes.png");
-          yes.setAttribute("alt", "Yes");
-          yes.setAttribute("class", "yes_icon");
-                yes.setAttribute("onclick", "yes(event,this);");
-                no.setAttribute("src", "Iconos/no.png");
-          no.setAttribute("alt", "No");
-          no.setAttribute("class", "no_icon");
-                no.setAttribute("onclick", "no(event,this);");
-                pencil2.setAttribute("src", "Iconos/pencil.png");
-                pencil2.setAttribute("alt", "Pencil;");
-                pencil2.setAttribute("class", "pencil2_icon");
-                    
-          div1.appendChild(img);
-                div4.appendChild(trash);
-                div5.appendChild(heart);
-                div6.appendChild(yes);
-                div7.appendChild(no);
-                div8.appendChild(h4);
-          h4.setAttribute("class", "trash_message");
-          h4.innerHTML = "You are about to delete this device. Continue? ";
-                h3.setAttribute("class", "door_name");
-          h3.innerHTML = $('#door_input').val();
-          div2.appendChild(h3);
-                div2.appendChild(pencil2);
-                div2.setAttribute("onmouseover", "pencil2_display(event,this);");
-                div2.setAttribute("onmouseout","pencil2_out(event,this);");   
-          p.setAttribute("class", "door_room");
-          p.innerHTML = $('#room_input').val();
-          var div9 = document.createElement("div");
-          var p1 = document.createElement("p");
-          p1.innerHTML = "Info on";
-          div9.setAttribute("class", "panel");
-                div9.appendChild(p1);
-          div3.appendChild(p);
-          elem.appendChild(div1);
-                
-                elem.appendChild(div5);
-                elem.appendChild(div4);
-                elem.appendChild(div6);
-                elem.appendChild(div7);
-                elem.appendChild(div2);
-                elem.appendChild(div3);
-                elem.appendChild(div8);
-               // elem.appendChild(div9);
-                
-          list.appendChild(elem);
-          list.appendChild(div9);
-                addPanels();
-
-            $(this).dialog('close');
-
-          }
-        }
-    ]
-  })
-});
+    }    
 
 });
