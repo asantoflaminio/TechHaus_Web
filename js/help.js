593816b091fcff090b28c9bac628a690fe0d3271
rociodellasala <32231675+rociodellasala@users.noreply.github.com>
2018-05-16 15:05:04 -0300

    Add files via upload

--- js/help.js
+++ js/help.js
@@ -1,60 +1,97 @@
-function showFavorites() {
-    // Declare variables
-    var logo,  favs, gs, dl, ad;
-    logo = document.getElementById("logogrande");
-    logo.style.display = "none";
-    gs = document.getElementById("gstarted");
-    gs.style.display = "none";
-    dl = document.getElementById("Delete");
-    dl.style.display = "none";
-    ad = document.getElementById("Adding");
-    ad.style.display = "none";
-    favs = document.getElementById("Favs");
-    favs.style.display = "block";
-}
-
 function showGettingStarted() {
     // Declare variables
-    var logo,  gs, favs, dl, ad;
+    var logo, gs, dv, rm, rt;
+    var curr = document.getElementsByClassName("current")[0];
+    if(curr === undefined);else{curr.removeAttribute("class", "current");}
+
+    
     logo = document.getElementById("logogrande");
     logo.style.display = "none";
-    favs = document.getElementById("Favs");
-    favs.style.display = "none";
-    dl = document.getElementById("Delete");
-    dl.style.display = "none";
-    ad = document.getElementById("Adding");
-    ad.style.display = "none";
+    
+    rm = document.getElementById("Rooms");
+    rm.style.display = "none";
+    
+    dv = document.getElementById("Devices");
+    dv.style.display = "none";
+    
+    rt = document.getElementById("Routines");
+    rt.style.display = "none";
+    
     gs = document.getElementById("gstarted");
     gs.style.display = "block";
+    document.getElementById("gs").setAttribute("class", "current");
     
 }
 
-function showDeleting() {
+function showRooms() {
     // Declare variables
-    var logo,  gs, favs, dl, ad;
+    var logo, gs, dv, rm, rt; 
+    var curr = document.getElementsByClassName("current")[0];
+    if(curr === undefined);else{curr.removeAttribute("class", "current");}
+
     logo = document.getElementById("logogrande");
     logo.style.display = "none";
-    favs = document.getElementById("Favs");
-    favs.style.display = "none";
+    
+    rm = document.getElementById("Rooms");
+    rm.style.display = "block";
+    document.getElementById("rm").setAttribute("class", "current");
+   
+    dv = document.getElementById("Devices");
+    dv.style.display = "none";
+
     gs = document.getElementById("gstarted");
     gs.style.display = "none";
-    ad = document.getElementById("Adding");
-    ad.style.display = "none";
-    dl = document.getElementById("Delete");
-    dl.style.display = "block";
+   
+    rt = document.getElementById("Routines");
+    rt.style.display = "none";
+    
 }
 
-function showAdding() {
+
+function showDevices() {
     // Declare variables
-    var logo,  gs, favs, dl, ad;
+    var logo, gs, dv, rm, rt; 
+   var curr = document.getElementsByClassName("current")[0];
+    if(curr === undefined);else{curr.removeAttribute("class", "current");}
+
     logo = document.getElementById("logogrande");
     logo.style.display = "none";
-    favs = document.getElementById("Favs");
-    favs.style.display = "none";
+    
+    rm = document.getElementById("Rooms");
+    rm.style.display = "none";
+    
+    dv = document.getElementById("Devices");
+    dv.style.display = "block";
+    document.getElementById("dv").setAttribute("class", "current");
+    
     gs = document.getElementById("gstarted");
     gs.style.display = "none";
-    dl = document.getElementById("Delete");
-    dl.style.display = "none";
-    ad = document.getElementById("Adding");
-    ad.style.display = "block";
+    
+    rt = document.getElementById("Routines");
+    rt.style.display = "none";
+    
 }
+
+function showRoutines() {
+    // Declare variables
+    var logo, gs, dv, rm, rt; 
+    var curr = document.getElementsByClassName("current")[0];
+    if(curr === undefined);else{curr.removeAttribute("class", "current");}
+
+    logo = document.getElementById("logogrande");
+    logo.style.display = "none";
+    
+    rm = document.getElementById("Rooms");
+    rm.style.display = "none";
+    
+    dv = document.getElementById("Devices");
+    dv.style.display = "none";
+   
+    gs = document.getElementById("gstarted");
+    gs.style.display = "none";
+
+    rt = document.getElementById("Routines");
+    rt.style.display = "block";
+    document.getElementById("rt").setAttribute("class", "current");
+}
+
