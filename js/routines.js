73a9ab52a7d6cbf489bc073b08569f580c205703
florgiorgi <mgiorgi@itba.edu.ar>
2018-05-16 15:45:11 -0300

    clear de popups

--- js/routines.js
+++ js/routines.js
@@ -284,7 +284,7 @@ function edit_routine(event, addbtn){
                 no = 1;
                 document.getElementById("device-tag22222").style.color = "#ff0000";
                 
-                document.getElementById("device-tag22222").innerHTML = "Please choose a device";
+                document.getElementById("device-tag22222").innerHTML = "Device is required";
                 $('#add_task_popup').modal('show');
         }else{
             document.getElementById("device-tag22222").style.color = "#000000";
@@ -293,7 +293,7 @@ function edit_routine(event, addbtn){
                 no = 1;
                 document.getElementById("action-tag22222").style.color = "#ff0000";
                 
-                document.getElementById("action-tag22222").innerHTML = "Please choose an action";
+                document.getElementById("action-tag22222").innerHTML = "Action is required";
                 $('#add_task_popup').modal('show');
         }else{
             document.getElementById("action-tag22222").style.color = "#000000";
@@ -312,6 +312,11 @@ function edit_routine(event, addbtn){
                                 api.routines.updateRoutine(rut_id, item2.name, pp).done(function(data){
                                   onPageLoad();
                                     $('#add_task_popup').modal('hide');
+                                    document.getElementById("action-tag22222").innerHTML = "Action*";
+                                    document.getElementById("device-tag22222").innerHTML = "Device*";
+                                    document.getElementById("device_input2").options[document.getElementById("device_input2").selectedIndex].text = "--";
+                                    document.getElementById("action_input2").options[document.getElementById("action_input2").selectedIndex].text = "--";
+                                
                                  }); 
                             }
                                 
@@ -395,7 +400,7 @@ function add_routine(event, name) {
         if(rut_name == ""){
                 no = 1;
                 document.getElementById("name-tag").style.color = "#ff0000";
-                document.getElementById("name-tag").innerHTML = "Please choose a name";
+                document.getElementById("name-tag").innerHTML = "Name is required";
                 $('#add_room_popup').modal('show');
         }else{
                 document.getElementById("name-tag").style.color = "#000000";
@@ -403,7 +408,7 @@ function add_routine(event, name) {
         if(device == "--" || device == "" || device == null){
                 no = 1;
                 document.getElementById("device-tag").style.color = "#ff0000";
-                document.getElementById("device-tag").innerHTML = "Please choose a device";
+                document.getElementById("device-tag").innerHTML = "Device is required";
                 $('#add_room_popup').modal('show');
         }else{
             document.getElementById("device-tag").style.color = "#000000";
@@ -411,7 +416,7 @@ function add_routine(event, name) {
         if(action == "--" || action == "" || action == null){
                 no = 1;
                 document.getElementById("action-tag").style.color = "#ff0000";
-                document.getElementById("action-tag").innerHTML = "Please choose an action";
+                document.getElementById("action-tag").innerHTML = "Action is required";
                 $('#add_room_popup').modal('show');
         }else{
             document.getElementById("action-tag").style.color = "#000000";
@@ -427,6 +432,12 @@ function add_routine(event, name) {
                                 
                                   onPageLoad();
                                     $('#add_room_popup').modal('hide');
+                                    document.getElementById("action-tag").innerHTML = "Action*";
+                                    document.getElementById("device-tag").innerHTML = "Device*";
+                                    document.getElementById("name-tag").innerHTML = "Name*";
+                                    document.getElementById("device_input").options[document.getElementById("device_input").selectedIndex].text = "--";
+                                    document.getElementById("action_input").options[document.getElementById("action_input").selectedIndex].text = "--";
+                                    document.getElementById("name_input").value = "";
                                  });
                 
                }
@@ -437,106 +448,7 @@ function add_routine(event, name) {
     
 }
 
-$(function() {
-    
-    function edit(event, title){
-    var divHtml = title.html();
-    var editableText = $("<textarea />");
-    editableText.val(divHtml);
-    title.replaceWith(editableText);
-    editableText.focus();
-    // setup the blur event for this new textarea
-    editableText.blur(editableTextBlurred(title));
-    }
 
-    function editableTextBlurred(title) {
-    var html = title.val();
-    var viewableText = $("<div>");
-    viewableText.html(html);
-    title.replaceWith(viewableText);
-    // setup the click event for this new div
-    $(viewableText).click(divClicked);
-    }
-    
-$("#addicon").click(function() {
-
-    $("#popup").dialog({
-        modal: true,
-        buttons: [ 
-            {   
-                text:'Cancel',
-                "class": "mybutton cancel-button",
-                click: function() {
-
-                $(this).dialog('close');
-                }
-            },
-            {
-                text: 'Add',
-                "class": "mybutton",
-                click: function () {
-
-                var list = document.getElementById("routines_list");
-                var elem = document.createElement("div");
-                elem.setAttribute("class", "accordion");
-                var div = document.createElement("div");
-                var img1 = document.createElement("img");
-                var img2 = document.createElement("img");
-                var h3 = document.createElement("h3");
-                var pnl = document.createElement("div");
-                var p = document.createElement("p");
-                pnl.setAttribute("class", "panel");
-                p.innerHTML = "This routine is empty!";
-                pnl.appendChild(p);
-                img1.setAttribute("src", "Iconos/arrow_down.png");
-                img1.setAttribute("alt", "Expand");
-                img1.setAttribute("class", "arrow_icon");
-
-                img2.setAttribute("src", "Iconos/play.png");
-                img2.setAttribute("alt", "Play");
-                img2.setAttribute("class", "play_icon");
-                img2.setAttribute("onclick", "play(this,event)");
-                    
-
-                h3.setAttribute("class", "routine_name");
-                h3.innerHTML = $('#routine_input').val();
-
-                div.appendChild(img1);
-                div.appendChild(img2);
-                div.appendChild(h3);
-                elem.appendChild(div);
-                list.appendChild(elem);
-                list.appendChild(pnl);
-                        elem.addEventListener("click", function() {
-                    /* Toggle between adding and removing the "active" class,
-                    to highlight the button that controls the panel */
-                    /*this.classList.toggle("active");*/
-
-                    /* Toggle between hiding and showing the active panel */
-                    var panel = this.nextElementSibling;
-                    if (panel.style.display === "block") {
-                        panel.style.display = "none";
-                    } else {
-                        panel.style.display = "block";
-                    }
-
-                    var arrow = this.children[0].children[0];
-
-                    if (panel.style.display === "block") {
-                        arrow.src = "Iconos/arrow_up.png";
-                    } else {
-                        arrow.src = "Iconos/arrow_down.png";
-                    }
-                });
-                $(this).dialog('close');
-
-                }
-            }
-        ]
-    })
-})
-
-});
 
 //delete routine
 
@@ -568,3 +480,24 @@ function delete_routine(event, confirm){
         onPageLoad(); 
     });
 };
+
+function cancel_routine(event, routine) {
+    document.getElementById("action-tag").innerHTML = "Action*";
+    document.getElementById("device-tag").innerHTML = "Device*";
+    document.getElementById("name-tag").innerHTML = "Name*";
+    document.getElementById("device_input").options[document.getElementById("device_input").selectedIndex].text = "--";
+    document.getElementById("action_input").options[document.getElementById("action_input").selectedIndex].text = "--";
+    document.getElementById("name_input").value = "";
+    document.getElementById("device-tag").style.color = "#000000";
+    document.getElementById("action-tag").style.color = "#000000";
+    document.getElementById("name-tag").style.color = "#000000";
+}
+
+function cancel_task(event, routine) {
+    document.getElementById("action-tag22222").innerHTML = "Action*";
+    document.getElementById("device-tag22222").innerHTML = "Device*";
+    document.getElementById("device_input2").options[document.getElementById("device_input2").selectedIndex].text = "--";
+    document.getElementById("action_input2").options[document.getElementById("action_input2").selectedIndex].text = "--";
+    document.getElementById("device-tag22222").style.color = "#000000";
+    document.getElementById("action-tag22222").style.color = "#000000";
+}
